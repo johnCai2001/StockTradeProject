8f9e2ba68a03be15e86b8a2a1ec616320d40adef
johncai <john10201021@gmail.com>
2026-03-12 16:57:22 +0800

    main structure

---
+++ src/main/java/Provider/MockNewsProvider.java
@@ -0,0 +1,5 @@
+package Provider;
+
+public class MockNewsProvider {
+
+}
---
+++ src/main/java/com/example/demo/StockTradeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTradeProjectApplication.class, args);
+	}
+
+}
